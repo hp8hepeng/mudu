5f2dae3ec8685e5a098ad4dc6e04558cf30832e2
hepeng <6789@jkl>
2018-05-12 23:41:36 +0800

    hepeng
    提交代码

---
+++ echartDate/jgcxjcl.js
@@ -0,0 +1,14 @@
+var arcLine = {
+    'ldata': ['去年', '今年'],
+    'xdata': ['0', '1', '2', '3', '4', '5'],
+    'data': [
+        [0, 132],
+        [0, 182],
+        [0, 232],
+        [0, 332]
+    ],
+    'color': ['#9785f1', '#26c6da', '#1e88e5', '#edba62'],
+    'isColor': ["itemStyle: { normal: {color: new echarts.graphic.LinearGradient(0, 0, 0, 1,[{offset: 0, color: '#41ccde'},{offset: 0.5, color: '#2ea6e2'},{offset: 1, color: '#238ce5'}])}}",
+        "itemStyle: { normal: {color: new echarts.graphic.LinearGradient(0, 0, 0, 1,[{offset: 0, color: '#9785f1'},{offset: 0.5, color: '#9677e3'},{offset: 1, color: '#9761cb'}])}}"
+    ]
+}
